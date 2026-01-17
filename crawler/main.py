a62f2bd83e6838e59bca400f8bd0820ec87d01c5
Long Nguyen <faker2407@gmail.com>
2026-01-17 16:34:36 +0700

    error handle - ssl

--- crawler/main.py
+++ crawler/main.py
@@ -3,12 +3,14 @@ import requests
 def crawl_headers(url):
     if not url.startswith("https://"):
         url = 'https://' + url
-    response = requests.head(url=url)
-    header = response.headers
-
-    print("Headers:")
-    for key, value in header.items():
-        print(f"{key}: {value}")
+    try:
+        response = requests.head(url=url, allow_redirects=True)
+        header = response.headers
+        print("Headers:")
+        for key, value in header.items():
+            print(f"{key}: {value}")
+    except requests.exceptions.SSLError:
+        print("SSL Error")
 
 if __name__ == "__main__":
     target_url = input("Enter URL: ")
